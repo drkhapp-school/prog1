5c7112e7ee12903b725f3f57116411070dcda049
DarkHappy <darkhappyy@gmail.com>
2020-10-08 14:46:22 -0400

    uwu?

--- travail-1/src/com/drkhapp/Main.java
+++ travail-1/src/com/drkhapp/Main.java
@@ -1,11 +1,3 @@
-/**
- * Objectif: Un jeu de nombre mystère consiste à trouver un nombre entre 1 et
- * 99, sélectionné au hasard, selon un nombre d’essais limités à cinq.
- *
- * @author Jean-Philippe Miguel-Gagnon
- * Session A2020
- */
-
 package com.drkhapp;
 
 import java.util.InputMismatchException;
@@ -13,20 +5,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Objectif: Un jeu de nombre mystère consiste à trouver un nombre entre 1 et
+     * 99, sélectionné au hasard, selon un nombre d’essais limités à cinq.
+     *
+     * @author Jean-Philippe Miguel-Gagnon
+     * Session A2020
+     */
     public static void main(String[] args) {
-        int nbMin; // plus petit nombre
-        int nbMax; // plus gros nombre
-        int nbMyst; // nombre à trouver
-        int nbLimit; // nombre de tentative
-        int nbEssaie; // tentative du joueur
+        int min; // plus petit nombre
+        int max; // plus gros nombre
+        int myst; // nombre à trouver
+        int limit; // nombre de tentative
+        int essaie; // tentative du joueur
         boolean reussi; // si la personne a réussi;
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
 
-        nbMin = 0;
-        nbMax = 100;
-        nbLimit = 1;
-        nbMyst = rand.nextInt(99) + 1;
+        min = 0;
+        max = 100;
+        limit = 1;
+        myst = rand.nextInt(99) + 1;
         reussi = false;
 
         System.out.println("--------------------------------");
@@ -36,31 +35,31 @@ public class Main {
 
         do {
             try {
-                System.out.print("(" + nbLimit + "/5) " + nbMin + " < ? < " + nbMax + " : ");
-                nbEssaie = scan.nextInt();
+                System.out.print("(" + limit + "/5) " + min + " < ? < " + max + " : ");
+                essaie = scan.nextInt();
 
-                if (nbEssaie < nbMyst)
-                    nbMin = nbEssaie;
-                else if (nbEssaie > nbMyst)
-                    nbMax = nbEssaie;
+                if (essaie < myst)
+                    min = essaie;
+                else if (essaie > myst)
+                    max = essaie;
                 else {
                     reussi = true;
                     break;
                 }
 
-                nbLimit++;
+                limit++;
             } catch (InputMismatchException e) {
                 System.out.println("Rentrer un entier.");
                 scan.next();
             }
-        } while (nbLimit <= 5);
+        } while (limit <= 5);
 
         System.out.println("\n--------------------------------");
 
         if (reussi)
-            System.out.println("Félicitation! Le nombre était: " + nbMyst);
+            System.out.println("Félicitation! Le nombre était: " + myst);
         else
-            System.out.println("Désolé! Le nombre était: " + nbMyst);
+            System.out.println("Désolé! Le nombre était: " + myst);
 
         scan.close();
     }
